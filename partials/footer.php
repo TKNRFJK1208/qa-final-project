d693bb8203c2e891094075bea4a572218885d235
takanori <takanori.fujiki@gmail.com>
2021-07-28 19:55:04 -0700

    add sections

---
+++ partials/footer.php
@@ -0,0 +1,28 @@
+<section id="footer">
+    <div class="footer container">
+      <div class="brand"></div>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="https://www.instagram.com/t8k81208/" title="email" target="_blank">
+          <img src="https://img.icons8.com/ios/50/A1A0A0/instagram-new--v1.png" alt="instagram" />
+          </a>
+        </div>
+        <div class="social-item">
+          <a href="https://github.com/TKNRFJK1208" title="github" target="_blank">
+            <img src="https://img.icons8.com/wired/64/A1A0A0/github.png" alt="github">
+          </a>
+        </div>
+        <div class="social-item">
+          <a href="https://www.linkedin.com/in/takanori-fujiki-575746195/" title="linkedin" target="_blank">
+            <img src="https://img.icons8.com/ios/50/A1A0A0/linkedin-circled--v2.png" alt="linkedin">
+          </a>
+        </div>
+      </div>
+      <p class="copyright">© 2020 Takanori Fujiki</p>
+    </div>
+  </section>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="./main.js"></script>
+</body>
+</html>
