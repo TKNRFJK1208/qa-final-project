dd339eff2e7453ded6606dabb34c32e22c09a9ff
takanori <takanori.fujiki@gmail.com>
2021-08-02 19:46:03 -0700

    add textbox and image-box as a frame in index.php

--- partials/footer.php
+++ partials/footer.php
@@ -3,22 +3,22 @@
       <div class="brand"></div>
       <div class="social-icon">
         <div class="social-item">
-          <a href="https://www.instagram.com/t8k81208/" title="email" target="_blank">
+          <a href="" title="email" target="_blank">
           <img src="https://img.icons8.com/ios/50/A1A0A0/instagram-new--v1.png" alt="instagram" />
           </a>
         </div>
         <div class="social-item">
-          <a href="https://github.com/TKNRFJK1208" title="github" target="_blank">
-            <img src="https://img.icons8.com/wired/64/A1A0A0/github.png" alt="github">
+          <a href="" title="twitter" target="_blank">
+            <img src="https://img.icons8.com/wired/64/A1A0A0/twitter-circled--v2.png" alt="twitter">
           </a>
         </div>
         <div class="social-item">
-          <a href="https://www.linkedin.com/in/takanori-fujiki-575746195/" title="linkedin" target="_blank">
-            <img src="https://img.icons8.com/ios/50/A1A0A0/linkedin-circled--v2.png" alt="linkedin">
+          <a href="https://www.linkedin.com/in/takanori-fujiki-575746195/" title="facebook" target="_blank">
+            <img src="https://img.icons8.com/ios/50/A1A0A0/facebook-circled--v2.png" alt="facebook">
           </a>
         </div>
       </div>
-      <p class="copyright">© 2020 Takanori Fujiki</p>
+      <p class="copyright">© 2020 Your Name</p>
     </div>
   </section>
 
